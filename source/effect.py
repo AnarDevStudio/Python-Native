a5e306ea2fb967206a1b7379728ddae3f6fcb8ca
AnarLabs <anardevstudio@gmail.com>
2026-02-18 18:40:42 +0400

    useEffect

---
+++ source/effect.py
@@ -0,0 +1,27 @@
+NO_DEPS = object()
+
+class Effect:
+    def __init__(self, function, deps=NO_DEPS): 
+        self.function = function
+        self.deps = deps
+        self.first_run = True
+        self.prev = None
+        self.execute()
+
+    def execute(self):
+        if self.deps is NO_DEPS:
+            self.function()
+            return
+        if self.deps is None:
+            if self.first_run:
+                self.function()
+                self.first_run = False
+            return
+        if self.prev != self.deps:
+            self.prev = self.deps
+            self.function()
+
+
+
+Effect(lambda: print("Her render"))
+
